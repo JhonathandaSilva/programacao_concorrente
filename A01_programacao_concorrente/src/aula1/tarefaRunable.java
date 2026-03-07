9cc8f47297a4212f4ef301b09c1bfeefa64a0975
jhonathandasilva <jhoni.nathan@icloud.com>
2026-03-06 21:21:55 -0300

    finalizacao aula 1

---
+++ A01_programacao_concorrente/src/aula1/tarefaRunable.java
@@ -0,0 +1,16 @@
+package aula1;
+
+public class tarefaRunable {
+	private String nome;
+	private int tempo;
+	
+	public tarefaRunable{
+		this.nome = nome;
+		this.tempo = tempo;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+}
